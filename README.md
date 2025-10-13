34612126140b88398752b615b4b4f731672d5351
Jon Durrant <jon@jondurrant.com>
2025-10-13 09:07:00 +0100

    Demo version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Waveshare RP2350PiZero Experiments
+
+Testing out the [Waveshare RP2350 PIZero](https://www.waveshare.com/rp2350-pizero.htm?&aff_id=DrJonEA) 
+for a review on my YouTube channel [DrJonEA](https://youtube.com/@drjonea).
+
+Experiments here are all compiled with the Pico SDK Version 2.2.0.  The libraries are those shipped by
+Waveshare, though a few bug fizes to get working on SDK 2.2.0 have been added.
---
+++ exp/drjonea/src/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/clocks.h"
+#include "hardware/irq.h"
+#include "hardware/sync.h"
+#include "hardware/gpio.h"
+#include "hardware/vreg.h"
+
+#include "dvi.h"
+#include "dvi_serialiser.h"
+#include "common_dvi_pin_configs.h"
+#include "tmds_encode.h" 
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "GUI_Paint.h"
+
+// DVDD 1.2V (1.1V seems ok too)
+#define FRAME_WIDTH 640
+#define FRAME_HEIGHT 480
+#define VREG_VSEL VREG_VOLTAGE_1_10
+#define DVI_TIMING dvi_timing_640x480p_60hz
+
+// RGB111 bitplaned framebuffer
+#define PLANE_SIZE_BYTES (FRAME_WIDTH * FRAME_HEIGHT / 8)
+#define Scale3_RED 0x4
+#define Scale3_GREEN 0x2
+#define Scale3_BLUE 0x1
+#define Scale3_BLACK 0x0
+#define Scale3_WHITE 0x7
+uint8_t framebuf[3 * PLANE_SIZE_BYTES];
+
+struct dvi_inst dvi0;
+
+void core1_main()
+{
+	dvi_register_irqs_this_core(&dvi0, DMA_IRQ_0);
+	dvi_start(&dvi0);
+	while(true)
+	{
+		for (uint y = 0; y < FRAME_HEIGHT; ++y)
+		{
+			uint32_t *tmdsbuf=0;
+			queue_remove_blocking_u32(&dvi0.q_tmds_free, &tmdsbuf);
+			for (uint component = 0; component < 3; ++component)
+			{
+				tmds_encode_1bpp(
+					(const uint32_t *)&framebuf[y * FRAME_WIDTH / 8 + component * PLANE_SIZE_BYTES],
+					tmdsbuf + component * FRAME_WIDTH / DVI_SYMBOLS_PER_WORD,
+					FRAME_WIDTH);
+			}
+			queue_add_blocking_u32(&dvi0.q_tmds_valid, &tmdsbuf);
+		}
+	}
+}
+
+int main()
+{
+	vreg_set_voltage(VREG_VSEL);
+	sleep_ms(10);
+	set_sys_clock_khz(DVI_TIMING.bit_clk_khz, true);
+
+	setup_default_uart();
+
+	pio_set_gpio_base(DVI_DEFAULT_SERIAL_CONFIG.pio,16);
+
+	dvi0.timing = &DVI_TIMING;
+	dvi0.ser_cfg = DVI_DEFAULT_SERIAL_CONFIG;
+	dvi_init(&dvi0, next_striped_spin_lock_num(), next_striped_spin_lock_num());
+	Paint_NewImage(framebuf, FRAME_WIDTH, FRAME_HEIGHT, 0, Scale3_WHITE);
+	Paint_SetScale(3);
+	multicore_launch_core1(core1_main);
+
+	Paint_Clear(Scale3_BLACK);
+	sleep_ms(500);
+
+	for (;;){
+
+		uint16_t rad = FRAME_HEIGHT/2-10;
+		for (int i=0; i < 6; i++){
+			Paint_DrawCircle(
+					FRAME_WIDTH/2,
+					FRAME_HEIGHT/2,
+					rad,
+					(uint16_t) i+1,
+					DOT_PIXEL_4X4,
+					DRAW_FILL_EMPTY);
+			rad = rad - 20;
+		}
+
+        Paint_DrawString_EN(FRAME_WIDTH/2-50, FRAME_HEIGHT/2-10, "DrJonEA", &Font20, 0xFFFF, 0x0000);
+        sleep_ms(5000);
+	}
+}
